25c8de109c47b911bcea297b19a2be16f63eeae8
MikhailKorotkov <mikh.korotkov1992@yandex.ru>
2022-05-26 18:11:35 +0300

    ''

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -12,7 +12,21 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Петя'},
 ]
-# ???
+
+def count(students):
+    students_count = {}
+    for student in students:
+        person = student.get('first_name')
+        if person not in students_count:
+            students_count[person] = 1
+        else:
+            students_count[person] += 1
+    return students_count
+
+students_count = count(students)
+for person, cnt in students_count.items():
+    print(f'{person}: {cnt}')
+
 
 
 # Задание 2
@@ -26,7 +40,23 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Оля'},
 ]
-# ???
+
+
+students_count = count(students)
+
+def most_popular(students):
+    person = None
+    most_popular = 0
+    for name, number in students.items():
+        if number > most_popular:
+            most_popular = number
+            person = name
+    return person
+
+print(f'Самое частое имя среди учеников: {most_popular(students_count)}')
+
+
+
 
 
 # Задание 3
@@ -51,7 +81,11 @@ school_students = [
         {'first_name': 'Саша'},
     ],
 ]
-# ???
+
+for ind, school_class in enumerate(school_students):
+    students_count = count(school_class)
+    most_popular_name = most_popular(students_count)
+    print(f'Самое популярное имя в классе {ind + 1}: {most_popular_name}')
 
 
 # Задание 4
@@ -60,10 +94,17 @@ school_students = [
 # Класс 2a: девочки 2, мальчики 0 
 # Класс 2б: девочки 0, мальчики 2
 
+# Задание 5
+# По информации о учениках разных классов нужно найти класс, в котором больше всего девочек и больше всего мальчиков
+# Пример вывода:
+# Больше всего мальчиков в классе 3c
+# Больше всего девочек в классе 2a
+
+
 school = [
-    {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
+    {'class': '2a', 'students': [{'first_name': 'Миша'}, {'first_name': 'Оля'}]},
     {'class': '2б', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-    {'class': '2б', 'students': [{'first_name': 'Даша'}, {'first_name': 'Олег'}, {'first_name': 'Маша'}]},
+    {'class': '2в', 'students': [{'first_name': 'Даша'}, {'first_name': 'Олег'}, {'first_name': 'Маша'}]},
 ]
 is_male = {
     'Олег': True,
@@ -72,24 +113,77 @@ is_male = {
     'Миша': True,
     'Даша': False,
 }
-# ???
 
 
-# Задание 5
-# По информации о учениках разных классов нужно найти класс, в котором больше всего девочек и больше всего мальчиков
-# Пример вывода:
-# Больше всего мальчиков в классе 3c
-# Больше всего девочек в классе 2a
+class Group():
 
-school = [
-    {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-    {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-]
-is_male = {
-    'Маша': False,
-    'Оля': False,
-    'Олег': True,
-    'Миша': True,
-}
-# ???
+    GROUPS = []
 
+    def __init__(self, group_number):
+        self.group = self.determine_group(group_number)
+        self.students = []
+        self.counter = {'М': 0, 'Ж': 0}
+
+    def determine_group(self, group_number):
+        return group_number
+
+    def __repr__(self):
+        return f'Класс {self.group}'
+
+    @classmethod
+    def add_gender_information(cls):
+        for group in cls.GROUPS:
+            for student in group.students:
+                if student.gender == 'М':
+                    group.counter['М'] += 1
+                else:
+                    group.counter['Ж'] += 1
+
+
+class Person(Group):
+
+    def __init__(self, name, group, gender):
+        super().__init__(group)
+        self.name = self.determine_name(name)
+        self.gender = self.determine_gender(gender)
+
+    def __repr__(self):
+        return f'(Имя: {self.name}, пол:{self.gender} )'
+
+    def determine_name(self, name):
+        return name
+
+    def determine_gender(self, gender):
+        if gender:
+            return 'М'
+        else: 
+            return 'Ж'
+
+
+def get_classes(school):
+    for group in school:
+        grp = Group(group['class'])
+        Group.GROUPS.append(grp)
+        for student in group['students']:
+            std = Person(student['first_name'], group['class'], is_male[student['first_name']])
+            grp.students.append(std)
+
+
+get_classes(school)
+Group.add_gender_information()
+
+
+girls_most = ['', 0]
+boys_most = ['', 0]
+for group in Group.GROUPS:
+    boys = group.counter['М']
+    girls = group.counter['Ж']
+    print(f'Класс {group.group}: мальчиков {boys}, девочек {girls}')
+    if boys > boys_most[1]:
+        boys_most[0] = group
+        boys_most[1] = boys
+    if girls > girls_most[1]:
+        girls_most[0] = group
+        girls_most[1] = girls
+print(f'Больше всего мальчиков в классе {boys_most[0].group}')
+print(f'Больше всего девочек в классе {girls_most[0].group}')
--- string_challenges.py
+++ string_challenges.py
@@ -1,28 +1,38 @@
 # Вывести последнюю букву в слове
 word = 'Архангельск'
-# ???
+print(word[-1])
 
 
 # Вывести количество букв "а" в слове
 word = 'Архангельск'
-# ???
+print(word.lower().count('а'))
 
 
 # Вывести количество гласных букв в слове
 word = 'Архангельск'
-# ???
+vowels = ('а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е')
+count = 0
+for letter in word.lower():
+    if letter in vowels:
+        count += 1
+print(count)
 
 
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
-# ???
+print(sentence.count(' ') + 1)
 
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-# ???
+ls = sentence.split(' ')
+for word in ls:
+    print(word[0])
 
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-# ???
+total_words = sentence.count(' ') + 1
+total_lenght = len(sentence.replace(' ', ''))
+average_length = total_lenght / total_words
+print(int(average_length))
